d1ff247cfd1a20ad9508923a36da1a1794812f88
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 15:24:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,9 +12,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
+      version = ">= 1.0"
     }
   }
 }
