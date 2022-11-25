0fe05f3a2a8d2d378fce8d2cbe081977fbad9585
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 11:44:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
-
 provider "azurerm" {
   features {}
 }
