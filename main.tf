35b8e4121a0d05b76b896757e9ed4e1f0b02e4c6
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 11:07:41 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,29 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_key_vault" "kv1" {
+  name                        = "testww1"
+  location                    = "East US"
+  resource_group_name         = "WW-CloudServiceManagement-RG-TBDNov30"
+  enabled_for_disk_encryption = true
+
+  tenant_id                  = "bdcfaa46-3f69-4dfd-b3f7-c582bdfbb820"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+
+  sku_name = "standard"
+}
