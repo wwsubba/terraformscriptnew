ecdf0f6d1da928e3958ccee71e732dd6ce791c7f
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 20:37:43 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -4,9 +4,9 @@ variable "resource_group_name" {
 }
 
 
+
 variable "tenant_id"
 {
   type        = string
   description = "azure resource group name"
-  default     = "bdcfaa46-3f69-4dfd-b3f7-c582bdfbb820"
 }
