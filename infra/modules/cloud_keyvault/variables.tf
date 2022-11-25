01e7751e145c87932ca006e24e89a52b0e22028c
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 20:51:46 +0530

    Update variables.tf

--- infra/modules/cloud_keyvault/variables.tf
+++ infra/modules/cloud_keyvault/variables.tf
@@ -2,10 +2,3 @@ variable "resource_group_name" {
   type        = string
   description = "azure resource group name"
 }
-
-
-variable "tenant_id"
-{
-  type        = string
-  description = "azure resource group name"
-}
