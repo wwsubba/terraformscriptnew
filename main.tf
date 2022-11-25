a88cff797ece3333d75c8d6846a2d161589a2f5c
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 19:07:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-data "azuread_client_config" "current" {}
-
-resource "azurerm_key_vault" "kv1" {
-  name                        = "testww12"
-  location                    = "East US"
-  resource_group_name         = "WW-CloudServiceManagement-RG-TBDNov30"
-  tenant_id                   = "bdcfaa46-3f69-4dfd-b3f7-c582bdfbb820"
-  enabled_for_disk_encryption = true
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-
-  sku_name = "standard"
+module "api-cloud-keyvault" {
+  source = "./modules/cloud_keyvault"
 }
