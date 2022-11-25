cf36ebe27f91b2b5fc733cc9e943be6e2702777a
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 18:42:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 3.0"
+  required_version = "1.2.0"
   cloud {
     organization = "wwsubba12"
 
@@ -10,7 +10,6 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0.0"
       use_msi = true
     }
   }
