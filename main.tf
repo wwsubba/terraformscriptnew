9211129d687f8eb5b8abb87acd8db7cc775d3ec3
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 15:02:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
-terraform {
-  cloud {
-    organization = "wwsubba12"
-
-    workspaces {
-      name = "wwsubbaapiworkspace"
-    }
-  }
-}
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">= 1.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-provider "azuread" {
-}
-
 data "azuread_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv1" {
