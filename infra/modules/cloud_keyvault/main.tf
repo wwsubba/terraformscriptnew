19ca302ff9f9795687bf99cab8485d6d3ad9e080
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 19:40:15 +0530

    Update main.tf

--- infra/modules/cloud_keyvault/main.tf
+++ infra/modules/cloud_keyvault/main.tf
@@ -1,7 +1,7 @@
 data "azuread_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv1" {
-  name                        = "testww12subba"
+  name                        = "testww12subba1"
   location                    = "East US"
   resource_group_name         = "WW-CloudServiceManagement-RG-TBDNov30"
   tenant_id                   = "bdcfaa46-3f69-4dfd-b3f7-c582bdfbb820"
