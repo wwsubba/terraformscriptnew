9c8e53e58c9d11d97c591bd7a21bf81f28934de3
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 19:25:29 +0530

    Create main.tf

---
+++ infra/modules/colud_keyvault/main.tf
@@ -0,0 +1,13 @@
+data "azuread_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv1" {
+  name                        = "testww12subba"
+  location                    = "East US"
+  resource_group_name         = "WW-CloudServiceManagement-RG-TBDNov30"
+  tenant_id                   = "bdcfaa46-3f69-4dfd-b3f7-c582bdfbb820"
+  enabled_for_disk_encryption = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
