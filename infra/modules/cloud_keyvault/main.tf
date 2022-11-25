598a319d4c60d1e1cf73d413a573b45f9df087b9
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 22:12:41 +0530

    Update main.tf

--- infra/modules/cloud_keyvault/main.tf
+++ infra/modules/cloud_keyvault/main.tf
@@ -3,7 +3,7 @@ data "azuread_client_config" "current" {}
 resource "azurerm_key_vault" "kv1" {
   name                        = "testww12subba1"
   location                    = "East US"
-  resource_group_name         = var.resource_group_name
+  resource_group_name         = "${var.resource_group_name}"
   tenant_id                   = "bdcfaa46-3f69-4dfd-b3f7-c582bdfbb820"
   enabled_for_disk_encryption = true
   soft_delete_retention_days  = 7
