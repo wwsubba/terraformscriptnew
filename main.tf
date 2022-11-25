242bad10e18c63f5db8ead7f8a3cb1c00feb3c25
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 15:41:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "azurerm_key_vault" "kv1" {
   name                        = "testww12"
   location                    = "East US"
   resource_group_name         = "WW-CloudServiceManagement-RG-TBDNov30"
+  tenant_id                   = [data.azurerm_client_config.current.tenant_id]
   enabled_for_disk_encryption = true
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
