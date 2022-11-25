06f13b577ac46af6d771f7dafe9d7069778200f4
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-25 22:59:42 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,7 @@
+terraform {
+  experiments = [module_variable_optional_attrs]
+}
+
 module "api-cloud-keyvault" {
   source = "./modules/cloud_keyvault"
   resource_group_name = var.resource_group_name
