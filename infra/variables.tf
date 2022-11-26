7e340cb0d58ce9852cca78fa15211db4e835a077
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-26 23:00:04 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,7 @@ variable "resource_group_name" {
   type        = string
   description = "base name of any resource"
 }
+variable "keyvault_name" {
+  type        = string
+  description = "name of key vault"
+}
