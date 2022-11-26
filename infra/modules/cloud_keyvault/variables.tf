032dbfb155382a8e585db7f8967884d2767093f8
wwsubba <118448489+wwsubba@users.noreply.github.com>
2022-11-26 23:01:42 +0530

    Update variables.tf

--- infra/modules/cloud_keyvault/variables.tf
+++ infra/modules/cloud_keyvault/variables.tf
@@ -2,3 +2,8 @@ variable "resource_group_name" {
   type        = string
   description = "base name of any resource"
 }
+
+variable "keyvault_name" {
+  type        = string
+  description = "name of key vault"
+}
